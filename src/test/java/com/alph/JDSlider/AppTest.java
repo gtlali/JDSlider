80a6d3765a7d4ae3c8f19341783394395f97fcf8
Gtl <gtlali@outlook>
2021-11-06 05:02:16 +0530

    updated pom.xml

--- src/test/java/com/alph/JDSlider/AppTest.java
+++ src/test/java/com/alph/JDSlider/AppTest.java
@@ -15,49 +15,45 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-        
-    }
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName
+	 *            name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	}
 
-    /**
-     * Rigourous Test :-)
-     * @throws MalformedURLException 
-     */
-    public void testApp() throws MalformedURLException {
-    	DesktopOptions options = new DesktopOptions();
-    	options.setApplicationPath("C:\\Windows\\System32\\calc.exe");
-    	
-    	WiniumDriver driver = new WiniumDriver(new URL("http://localhost:9999"), options); 	
-    	driver.findElement(By.name("6")).click();
-    	driver.findElement(By.id("92")).click();
-    	driver.findElement(By.name("8")).click();
-    	driver.findElement(By.name("Equals")).click();
-    	String s = driver.findElement(By.id("150")).getAttribute("Name");
-    	if (s.equals("48"))
-    		assertTrue( true );
-    	else
-    		assertFalse(true);
-    	
-    	
-    
-       //assertTrue( true );
-    }
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 * 
+	 * @throws MalformedURLException
+	 */
+	public void testApp() throws MalformedURLException {
+		DesktopOptions options = new DesktopOptions();
+		options.setApplicationPath("C:\\Windows\\System32\\calc.exe");
+
+		WiniumDriver driver = new WiniumDriver(new URL("http://localhost:9999"), options);
+		driver.findElement(By.name("6")).click();
+		driver.findElement(By.id("92")).click();
+		driver.findElement(By.name("8")).click();
+		driver.findElement(By.name("Equals")).click();
+		String s = driver.findElement(By.id("150")).getAttribute("Name");
+		if (s.equals("48"))
+			assertTrue(true);
+		else
+			assertFalse(true);
+
+	    assertTrue( true );
+	}
 }
